1bec047a9c72e1c50b560c47469bfa5f0595e188
22091397048 Fauzan Hedar Abit <124507966+Fauzan0104@users.noreply.github.com>
2024-04-25 08:23:31 +0700

    Add files via upload

---
+++ Laravel/app/Http/Controllers/Auth/ResetPasswordController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Http\Request;
+
+class ResetPasswordController extends Controller
+{
+    use ResetsPasswords;
+
+    protected $redirectTo = '/';
+
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
+
+    protected function rules()
+    {
+        return [
+            'token' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|confirmed|min:8',
+        ];
+    }
+
+    protected function validationErrorMessages()
+    {
+        return [];
+    }
+
+    protected function credentials(Request $request)
+    {
+        return $request->only(
+            'email', 'password', 'password_confirmation', 'token'
+        );
+    }
+
+    protected function resetPassword($user, $password)
+    {
+        $user->password = bcrypt($password);
+        $user->save();
+        // Atau gunakan metode setPasswordAttribute pada model User
+    }
+}
